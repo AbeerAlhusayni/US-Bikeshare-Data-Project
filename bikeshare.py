8e24318a00658002eebd0bd3ab310820ccfc25a9
AbeerAlhusayni <alhusayni.a@gmail.com>
2021-02-15 13:12:08 +0300

    refactor: Handle invalid inputs for city

--- bikeshare.py
+++ bikeshare.py
@@ -17,8 +17,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-    while True:
+while True:
         city = input("\n Would you like to see data for Chicago, New York, or Washington?\n").lower().strip()
 
         if city in ['chicago', 'new york', 'washington']:
